e3592057789ab1692eb1fdd3ec6fb3640725d2f4
Lucas Pinheiro <pinheiro.lucash@gmail.com>
2024-11-04 14:49:46 -0300

    criando a sessão de projetos e o componente de projetos

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,15 @@
+import Paragrafo from "../Paragraph"
+import Titulo from "../Title"
+import { Card, LinkBotao } from "./styles"
+
+const Projeto = () => (
+  <Card>
+    <Titulo>Projeto Lista da Tarefas</Titulo>
+    <Paragrafo tipo="secundario">
+      Lista de tarefas feitas com VueJS
+    </Paragrafo>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+
+export default Projeto
